51fc65146253e2d3e860a2e49db277992d9bb353
Ahmet <ahmetarapaslan@gmail.com>
2024-05-24 17:37:17 +0300

    feature: basket list remove button

--- src/components/HomeHeader/index.jsx
+++ src/components/HomeHeader/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { FaShoppingBasket } from "react-icons/fa";
+import { IoIosRemoveCircleOutline } from "react-icons/io";
 
 export default function HomeHeader({ money, totalPrice, basket, setBasket }) {
+  const removeBasket = (id) => {
+    const checkBasket = basket.find((item) => item.id === id);
+    checkBasket.amount -= 1;
+    if (checkBasket.amount === 0) {
+      setBasket([...basket.filter((item) => item.id !== id)]);
+    } else {
+      setBasket([...basket.filter((item) => item.id !== id), checkBasket]);
+    }
+  };
   return (
     <>
       {totalPrice > 0 && money - totalPrice !== 0 && (
@@ -24,20 +34,37 @@ export default function HomeHeader({ money, totalPrice, basket, setBasket }) {
                   basket.length > 5 ? "h-48" : ""
                 }   overflow-auto no-scrollbar`}
               >
-                <h3 className="text-center mb-3 text-xl sticky top-0 backdrop-blur-sm ">Sepetim</h3>
+                <h3 className="text-center mb-3 text-xl sticky top-0 backdrop-blur-sm ">
+                  Sepetim
+                </h3>
                 {basket.map((item) => (
-                  <div className="mt-2">
-                    <div className=" grid grid-cols-3 w-[150px] items-center justify-center mb-3  ">
+                  <div className="mt-2  ">
+                    <div
+                      key={item.id}
+                      className=" grid grid-cols-custom w-[160px] items-center justify-center mt-3 mb-2 border-b-2 border-white last:border-none"
+                    >
                       <div className="text-left">{item.title}</div>
-                      <div className="text-center">{item.amount} Adet</div>
-                      <div className=" text-right">
+                      <div className="text-right">{item.amount} Adet</div>
+                      <div className=" text-right ">
                         {item.price * item.amount} TL
+                      </div>
+                      <div
+                        onClick={() => removeBasket(item.id)}
+                        className="flex justify-end cursor-pointer select-none"
+                      >
+                        <IoIosRemoveCircleOutline className="h-4 w-4" />
                       </div>
                     </div>
                   </div>
                 ))}
-                <div className="flex justify-end mt-4">
-                  Toplam {totalPrice} TL
+                <div className="flex gap-x-3 mt-3 mb-3 items-center">
+                  <button
+                    onClick={() => setBasket([])}
+                    className="border px-1 py-0.5"
+                  >
+                    Sepeti boşalt
+                  </button>
+                  <div className="">Toplam {totalPrice} TL</div>
                 </div>
               </div>
             </div>
@@ -70,11 +97,44 @@ export default function HomeHeader({ money, totalPrice, basket, setBasket }) {
           <div className="sticky top-0 w-2/3  h-20 backdrop-blur-sm  text-center flex justify-center items-center uppercase text-xl font-bold">
             Hesabınızdaki tüm parayı harcadınız
           </div>
-          <div className=" relative ">
+          <div className=" relative group ">
             <FaShoppingBasket className="w-6 h-6" />
             <span className=" bg-black text-white h-4 w-4 flex text-xs rounded-full absolute top-4 left-4 items-center justify-center text-center ">
               {basket.length}
             </span>
+            <div className=" opacity-0 group-hover:opacity-100   duration-300  absolute top-4 left-3 bg-black px-6 py-3 rounded-md">
+              <div
+                className={`text-white text-xs text-nowrap ${
+                  basket.length > 5 ? "h-48" : ""
+                }   overflow-auto no-scrollbar`}
+              >
+                <h3 className="text-center mb-3 text-xl sticky top-0 backdrop-blur-sm ">
+                  Sepetim
+                </h3>
+                {basket.map((item) => (
+                  <div className="mt-2  ">
+                    <div
+                      key={item.id}
+                      className=" grid grid-cols-4 w-[160px] items-center justify-center mb-2 border-b-2 border-white last:border-none"
+                    >
+                      <div className="text-left">{item.title}</div>
+                      <div className="text-center">{item.amount} Adet</div>
+                      <div className=" text-right">
+                        {item.price * item.amount} TL
+                      </div>
+                      <div>
+                        {" "}
+                        <IoIosRemoveCircleOutline />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                <div className="flex">
+                  <button>Sepeti boşalt</button>
+                  <div className="">Toplam {totalPrice} TL</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       )}
